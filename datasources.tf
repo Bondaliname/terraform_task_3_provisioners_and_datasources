b7420a32338583247eed12a323dd2b6b801f4aec
Alina Bondarenko <alinamoskovchuk18@gmail.com>
2025-09-27 22:00:08 +0300

    2

--- datasources.tf
+++ datasources.tf
@@ -1,3 +1,24 @@
 data "azurerm_resource_group" "example" {
-  name = "tfvmex-resources"
+  name = "example-resources"
+}
+
+data "azurerm_virtual_network" "main" {
+  name                = "example-network"
+  resource_group_name = data.azurerm_resource_group.example.name
+}
+
+data "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = data.azurerm_resource_group.example.name
+  virtual_network_name = data.azurerm_virtual_network.main.name
+}
+
+data "azurerm_network_interface" "main" {
+  name                = "example-nic"
+  resource_group_name = data.azurerm_resource_group.example.name
+}
+
+data "azurerm_public_ip" "main" {
+  name                = "example-ip"
+  resource_group_name = data.azurerm_resource_group.example.name
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,34 @@
 variable "prefix" {
   default = "tfvmex"
 }
+
+variable "vm_name" {
+  type = string
+}
+
+variable "ssh_user" {
+  type    = string
+  default = "example-admin"
+}
+
+variable "ssh_private_key_path" {
+  type    = string
+  default = "~/.ssh/id_rsa"
+}
+
+variable "ssh_password" {
+  type    = string
+  default = "Password1234!"
+}
+
+variable "vnet_name" {
+  type = string
+}
+
+variable "subnet_name" {
+  type = string
+}
+
+variable "nic_name" {
+  type = string
+}
